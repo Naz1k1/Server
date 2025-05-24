7acdd7c917e908d6255257c587e59a7a29c76df0
Naz1k1 <yakosang2001@163.com>
2025-05-25 00:00:49 +0800

    common添加异常处理、封装响应体、logback日志。

--- server-backend/server-common/src/main/java/com/naz1k1/exception/GlobalExceptionHandler.java
+++ server-backend/server-common/src/main/java/com/naz1k1/exception/GlobalExceptionHandler.java
@@ -4,6 +4,11 @@ import com.naz1k1.model.response.R;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+/**
+ * 全局异常处理器
+ * @author Naz1k1
+ * @version 1.0.0
+ */
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     // 处理业务异常
